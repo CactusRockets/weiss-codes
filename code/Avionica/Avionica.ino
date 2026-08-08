a2e5288e05081769f22fc0d1a3279e0ed5fa9c90
Miller <guimigc@protonmail.com>
2026-08-08 13:39:48 -0300

    Revert "Arquivis buzzer.h e bmp.h atualizados"
    This reverts commit c9667c1efb2cc357be50dfddbc8b86e8e17871ad.
    
    Voltando para versão anterior

--- code/Avionica/Avionica.ino
+++ code/Avionica/Avionica.ino
@@ -262,5 +262,5 @@ void loop()
       }
     }
   }
-  delay(500);
+  delay(200);
 }
--- code/Avionica/buzzer.h
+++ code/Avionica/buzzer.h
@@ -1,53 +1,39 @@
-/* CONFIGURAÇÕES BUZZER PASSIVO - VOLUME MÁXIMO / MODO RESGATE */
+/* CONFIGURAÇÕES BUZZER */
 
 #define BUZZER_PIN 14
-
-
-#define BEEP_FREQ_MAX 3500 
-#define BEEP_TIME 300      
-#define BEEP_PAUSE 150
+#define BEEP_TIME 300
 
 void setupBuzzer() {
   pinMode(BUZZER_PIN, OUTPUT);
-  digitalWrite(BUZZER_PIN, LOW);
-  Serial.println("Buzzer (Modo Resgate) configurado!");
+  Serial.println("Buzzer configurado!");
 }
 
 void activateBuzzer() {
-  tone(BUZZER_PIN, BEEP_FREQ_MAX);
-  Serial.println("Buzzer ativado (Volume Max)");
+  digitalWrite(BUZZER_PIN, HIGH);
+  flash_up();
+  Serial.println("Buzzer ativado!");
 }
 
 void desactivateBuzzer() {
-  noTone(BUZZER_PIN);
   digitalWrite(BUZZER_PIN, LOW);
-  Serial.println("Buzzer desativado");
+  flash_down();
+  Serial.println("Buzzer desativado!");
 }
 
-// --- 1. Sinal Sonoro de Inicialização (3 Bips Estridentes) ---
 void tripleBuzzerBip() {
-  Serial.println("--> AVISO: Sistema Ligado! Emitindo 3 bips no volume máximo...");
+  activateBuzzer();
+  delay(BEEP_TIME);
+  desactivateBuzzer();
+  delay(BEEP_TIME);
+
+  activateBuzzer();
+  delay(BEEP_TIME);
+  desactivateBuzzer();
+  delay(BEEP_TIME);
   
-  for(int i = 0; i < 3; i++) {
-    tone(BUZZER_PIN, BEEP_FREQ_MAX);
-    delay(BEEP_TIME);
-    noTone(BUZZER_PIN);
-    digitalWrite(BUZZER_PIN, LOW);
-    delay(BEEP_PAUSE);
-  }
+  activateBuzzer();
+  delay(BEEP_TIME);
+  desactivateBuzzer();
+  delay(BEEP_TIME);
 }
 
-// --- 2. Sinal de Busca/Resgate (Sirene de Localização) ---
-// Chame essa função no loop() caso a aviônica detecte o pouso ou perda de sinal!
-void beaconLostMode() {
-  for(int freq = 2500; freq <= 4000; freq += 100) {
-    tone(BUZZER_PIN, freq);
-    delay(10);
-  }
-  for(int freq = 4000; freq >= 2500; freq -= 100) {
-    tone(BUZZER_PIN, freq);
-    delay(10);
-  }
-  noTone(BUZZER_PIN);
-  digitalWrite(BUZZER_PIN, LOW);
-}
